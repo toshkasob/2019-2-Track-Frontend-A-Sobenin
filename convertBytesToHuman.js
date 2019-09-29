5222d705c1251bf706ff6d89c3bc397b50bc8757
toshkasob <toshka_sob@mail.ru>
2019-09-30 00:37:12 +0300

    повтор №2 коммита, в котором весь проект с созданными функциями convertBytesToHuman.js, correctSentence.js, nonUniqueElements.js и тестами convertBytesToHuman.test.js

---
+++ convertBytesToHuman.js
@@ -0,0 +1,62 @@
+/*
+ * В этом задании надо разработать функцию
+ * `convertBytesToHuman`. Эта функция  должна принимать
+ * аргумент `bytes` только числового типа.
+ * На выходе функция должна отдать
+ * человекопонятную строку, которая будет
+ * отражать размер файла. Примеры использования:
+ * `convertBytesToHuman(1024) === '1 KB';`
+ * `convertBytesToHuman(123123123) === '117.42 MB';`
+ * Необходимо предусмотреть защиту от
+ * передачи аргументов неправильного типа
+ * и класса (например, отрицательные числа)
+ */
+
+export default function convertBytesToHuman(bytes) {
+  // your solution goes here
+
+  /*if (bytes.length == 0)
+    return false;
+  */
+  let typeBytes = typeof bytes;
+  let flagIsBytesNumber = (typeBytes == typeof(1));
+  if (flagIsBytesNumber == false) 
+  {
+    //alert(`[In] Types of bytes (${typeBytes}) is incorrect`);
+    return false;
+  } else if (isNaN(bytes))
+  {
+    //alert (`[In] bytes(${bytes}) is NaN`);
+    return false;
+  } else if (!isFinite(bytes))
+  {
+    //alert (`[In] bytes(${bytes}) is NaN`);
+    return false;
+  } else if (bytes < 0) 
+  {
+    //alert (`[In] bytes(${bytes}) is negative`);
+    return false;
+  }
+  
+  /*if (!isFinite(bytes))
+  {
+    return false;
+  } else if (bytes < 0) 
+  {
+    //alert (`[In] bytes(${bytes}) is negative`);
+    return false;
+  }
+  */
+  let arr = ["B", "KB", "MB", "GB", "TB"];
+  let index2suffixBytes = 0;
+  for (let index = 0; index < arr.length; index++) {
+    if (Math.floor(bytes / (1024**index) ) === 0) break;
+    index2suffixBytes = index;
+  }
+  let outString =  `convertBytesToHuman(${bytes}) === `;
+  outString += `'${Math.round( (bytes / (1024**index2suffixBytes)) * 100) / 100} `;
+  outString += `${arr[index2suffixBytes]}';`;
+  //alert (`[Out] ${outString}`);
+  return outString;
+
+}
---
+++ convertBytesToHuman.test.js
@@ -0,0 +1,65 @@
+/*
+ * Необходимо покрыть все возможные
+ * и невозможные кейсы. Например,
+ * convertBytesToHuman(-1) === false,
+ * convertBytesToHuman(-1) !== '1 B',
+ * convertBytesToHuman('string') === false
+ * convertBytesToHuman(5) === '5 B'
+ */
+
+import convertBytesToHuman from './convertBytesToHuman';
+
+test('Возвращает false для неправильного типа данных', () => {
+  expect(convertBytesToHuman('testString')).toBe(false)
+  expect(convertBytesToHuman(NaN)).toBe(false)
+  expect(convertBytesToHuman(-1)).toBe(false)
+  expect(convertBytesToHuman(-1023)).toBe(false)
+  expect(convertBytesToHuman([25,12,1024])).toBe(false)
+  expect(convertBytesToHuman(null)).toBe(false)
+  expect(convertBytesToHuman(alert())).toBe(false)
+  expect(convertBytesToHuman(true)).toBe(false)
+  expect(convertBytesToHuman(undefined)).toBe(false)
+  expect(convertBytesToHuman("Мерседес выигрывает F1 GP Russua in Sochi в 6 раз")).toBe(false)
+  expect(convertBytesToHuman(/* ... */)).toBe(/* ... */false)
+  // ...
+});
+
+test('Возвращает корректное значение для чисел', () => {
+  //expect(convertBytesToHuman(/* ... */)).toBe(/* ... */)
+  expect(convertBytesToHuman(0)).toBe(`convertBytesToHuman(0) === '0 B';`)
+  expect(convertBytesToHuman(1)).toBe(`convertBytesToHuman(1) === '1 B';`)
+  expect(convertBytesToHuman(1023)).toBe(`convertBytesToHuman(1023) === '1023 B';`)
+  expect(convertBytesToHuman(1024)).toBe(`convertBytesToHuman(1024) === '1 KB';`)
+  expect(convertBytesToHuman(1025)).toBe(`convertBytesToHuman(1025) === '1 KB';`)
+  expect(convertBytesToHuman(524288)).toBe(`convertBytesToHuman(524288) === '512 KB';`)
+  expect(convertBytesToHuman(1048576)).toBe(`convertBytesToHuman(1048576) === '1 MB';`)
+  expect(convertBytesToHuman(1048570)).toBe(`convertBytesToHuman(1048570) === '1023.99 KB';`)
+  expect(convertBytesToHuman(1048571)).toBe(`convertBytesToHuman(1048571) === '1024 KB';`)
+  expect(convertBytesToHuman(1053818)).toBe(`convertBytesToHuman(1053818) === '1 MB';`)
+  expect(convertBytesToHuman(1053819)).toBe(`convertBytesToHuman(1053819) === '1.01 MB';`)
+  expect(convertBytesToHuman(1572864)).toBe(`convertBytesToHuman(1572864) === '1.5 MB';`)
+  expect(convertBytesToHuman(3221225472)).toBe(`convertBytesToHuman(3221225472) === '3 GB';`)
+  expect(convertBytesToHuman(13491007672811)).toBe(`convertBytesToHuman(13491007672811) === '12.27 TB';`)
+  expect(convertBytesToHuman(1349100767281)).toBe(`convertBytesToHuman(1349100767281) === '1.23 TB';`)
+  expect(convertBytesToHuman(134910076728)).toBe(`convertBytesToHuman(134910076728) === '125.64 GB';`)
+  expect(convertBytesToHuman(1064910076)).toBe(`convertBytesToHuman(1064910076) === '1015.58 MB';`)
+  // ...
+});
+
+// другая группа проверок
+test('Не выдает значение для некорретных чисел', () => {
+  expect(convertBytesToHuman('testString')).not.toBe(`convertBytesToHuman(testString) === 'NaN B';`)
+  expect(convertBytesToHuman(-1023)).not.toBe(`convertBytesToHuman(1023) === '1023 B';`)
+  expect(convertBytesToHuman(-1023)).not.toBe(`convertBytesToHuman(-1023) === '-1023 B';`)
+  expect(convertBytesToHuman(-1023)).not.toBe(`convertBytesToHuman(-1023) === '1023 B';`)
+  expect(convertBytesToHuman(-1023)).not.toBe(`convertBytesToHuman(1023) === '-1023 B';`)
+  expect(convertBytesToHuman(null)).not.toBe(`convertBytesToHuman(0) === '0 B';`)
+  expect(convertBytesToHuman(null)).not.toBe(`convertBytesToHuman(null) === '0 B';`)
+  expect(convertBytesToHuman(alert())).not.toBe(`convertBytesToHuman(undefined) === 'NaN B';`)
+  expect(convertBytesToHuman(true)).not.toBe(`convertBytesToHuman(true) === '0 B';`)
+  expect(convertBytesToHuman(true)).not.toBe(`convertBytesToHuman(true) === 'NaN B';`)
+  expect(convertBytesToHuman(undefined)).not.toBe(`convertBytesToHuman(undefined) === 'NaN B';`)
+  expect(convertBytesToHuman("1023")).not.toBe(`convertBytesToHuman(1023) === '1023 B';`)
+  expect(convertBytesToHuman(/* ... */)).not.toBe(/* ... */`convertBytesToHuman(undefined) === 'NaN B';`)
+  // ...
+});
