60ea3a1733ebf19c3c132372e6bdf17c2705f732
toshkasob <toshka_sob@mail.ru>
2019-12-11 00:59:16 +0300

    put page 'Edit Profile' into /edit_profile

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -6,24 +6,30 @@ import MessageForm from '../components/MessageForm';
 import '../styles/globalStyles.css';
 import ChatHeader from '../components/ChatHeader';
 import ChatsListHeader from '../components/ChatsListHeader';
+import EditProfile from '../components/EditProfile';
+import EditProfileHeader from '../components/EditProfileHeader';
 
 export default class AppContainer extends React.Component {
-	render() {
-		return (
-			<div className="my_messenger">
-				<Router>
-					<Switch>
-						<Route path="/chat_id=:chatId">
-							<ChatHeader />
-							<MessageForm />
-						</Route>
-						<Route exact path="/">
-							<ChatsListHeader />
-							<ChatsList />
-						</Route>
-					</Switch>
-				</Router>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="my_messenger">
+        <Router>
+          <Switch>
+            <Route path="/chat_id=:chatId">
+              <ChatHeader />
+              <MessageForm />
+            </Route>
+            <Route exact path="/">
+              <ChatsListHeader />
+              <ChatsList />
+            </Route>
+            <Route path="/edit_profile">
+              <EditProfileHeader />
+              <EditProfile />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
