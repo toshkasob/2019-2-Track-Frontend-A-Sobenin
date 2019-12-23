d971c74d9cb6e1e3a15d28d27618aef47c8fda5b
toshkasob <toshka_sob@mail.ru>
2019-12-23 21:50:13 +0300

    запрос на получение погоды от сервера

---
+++ exam/src/components/fetchWeather.js
@@ -0,0 +1,22 @@
+
+import React, { useState } from 'react';
+
+let API_KEY = '39961b76c3604fc4d4d4422d057d1c1b';
+
+export default function fetchWeather(props){
+    const startURL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&`
+    const curProps = props;
+    let endURL = '';
+    if (curProps.method=='byCity') {
+        endURL = `q=${curProps.city}`;
+    } else if (curProps.method=='byGeo') {
+        endURL = `lat=${curProps.lat}&lon=${curProps.lon}`
+    }
+
+    const response = fetch(startURL + endURL, {
+        method: 'GET',
+    });
+    const weatherJson = response.json();
+    props.weather = weatherJson;
+
+}
