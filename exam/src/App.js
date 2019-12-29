4c11c087643771bc1746743894ab6ff7779eb678
toshkasob <toshka_sob@mail.ru>
2019-12-29 22:23:06 +0300

    попытка получить погоду для текущего местоположения

--- exam/src/App.js
+++ exam/src/App.js
@@ -15,15 +15,30 @@ export default class App extends React.Component{
       cityID = [524901, 571476, 534595, 7261214];
       window.localStorage.setItem('cityID', JSON.stringify(cityID));
     }
+    // let { latitude, longitude } = this.getGeo();
+    // if (latitude === undefined || latitude === null) {
+    //  latitude = 361;
+    //  longitude = 361; 
+    // }
+    // debugger;
     this.state = {
       weather: {
         cityID: cityID,
         currentWeather: [],
       },
+      geo: {
+        latitude: 361,
+        longitude: 361,
+      }
     }
   }
+
   componentDidMount() {
     // debugger;
+    // this.getGeo();
+    // this.getWeatherByGEO(this.state.geo);
+    this.getWeatherByGEO();
+
     this.getWeatherByID();
   }
   getWeatherByID() {
@@ -47,6 +62,76 @@ export default class App extends React.Component{
   
     }
   }
+  // getWeatherByGEO(position) {
+  getWeatherByGEO() {
+
+    debugger;
+    let latitude = 361;
+    let longitude = 361;
+    this.getGeo(latitude, longitude);
+    debugger;
+    if (Math.abs(latitude) >360 || Math.abs(longitude) >360 )
+      return;
+    const startURL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&`
+    let endURL = `lat=${latitude}&lon=${longitude}`;
+    debugger;
+    const fullURL = startURL + endURL + '&units=metric';
+    fetch(fullURL)
+      .then(response => response.json())
+      .then(json_data => (
+        this.setState((state) => {
+          // debugger;
+          let tmp_cityID = [json_data.id, state.weather.cityID];
+          let tmp_currentWeather = [json_data, state.weather.currentWeather];
+          state.weather.cityID = tmp_cityID;
+          state.weather.currentWeather = tmp_currentWeather;
+          return state;
+        })
+      ));
+  }
+
+  getGeo(latitude, longitude) {
+    if (!navigator.geolocation) {
+      // alert('Geolocation is not supported by your browser =(');
+      console.log('Geolocation is not supported by your browser =(');
+    } else {
+      const geoSuccess = (position) => {
+                const coords = [position.coords.latitude, position.coords.longitude];
+
+                // const { latitude, longitude } = position.coords;
+                debugger;
+                latitude = position.coords.latitude;
+                longitude = position.coords.longitude;
+                // return position.coords;
+                // this.setState( (state) => {
+                //   // cur_state = state.weather;
+                //   debugger;
+                //   state.geo.latitude = position.coords.latitude;
+                //   state.geo.longitude = position.coords.longitude;
+                //   return state;
+                // })
+                // this.getWeatherByGEO(position.coords);
+      };
+  
+      const geoError = (error) => {
+        // alert('Unable to retrieve your location... Try again =)');
+        console.log(error.message);
+      };
+  
+      const geoOptions = {
+        enableHighAccuracy: true,
+        maximumAge: 60000,
+        timeout: 15000,
+      };
+  
+      navigator.geolocation.getCurrentPosition(
+        geoSuccess,
+        geoError,
+        geoOptions,
+      );
+    }
+  }
+  
   render (){
     return (
       <div className="App">
@@ -63,3 +148,4 @@ export default class App extends React.Component{
     );
   }
 }
+
