861d830682014d6793b2d57aa9789bc239cdcb71
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-19 07:39:16 +0000

    Deploy toshkasob/2019-2-Track-Frontend-A-Sobenin to github.com/toshkasob/2019-2-Track-Frontend-A-Sobenin.git:gh-pages

---
+++ precache-manifest.6f39102142f75ffe043b9ac77f22eeac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ba9b1ee4b33b60dafc5824dd257f5f",
+    "url": "/2019-2-Track-Frontend-A-Sobenin/index.html"
+  },
+  {
+    "revision": "6fd2e226e04bf34d3fd4",
+    "url": "/2019-2-Track-Frontend-A-Sobenin/static/css/main.0354bb90.chunk.css"
+  },
+  {
+    "revision": "d0aa48cdce8fccea042d",
+    "url": "/2019-2-Track-Frontend-A-Sobenin/static/js/2.a3422d44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2019-2-Track-Frontend-A-Sobenin/static/js/2.a3422d44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fd2e226e04bf34d3fd4",
+    "url": "/2019-2-Track-Frontend-A-Sobenin/static/js/main.5ac83612.chunk.js"
+  },
+  {
+    "revision": "1884d47ed4dd453cf24e",
+    "url": "/2019-2-Track-Frontend-A-Sobenin/static/js/runtime-main.d64a4808.js"
+  }
+]);
